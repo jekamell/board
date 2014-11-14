2ce7ad7cf057eae67f3ab16dfd022e0fa8405359
Evgeniy Malykhin <55jeka@gmail.com>
2014-11-14 23:17:38 +0200

    Change: App: using overriding params in local config for console application

--- protected/config/main.php
+++ protected/config/main.php
@@ -43,19 +43,13 @@ return array(
             ),
         ),
         */
-        'db' => array(
-            'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/testdrive.db',
-        ),
-        // uncomment the following to use a MySQL database
-        /*
         'db'=>array(
             'connectionString' => 'mysql:host=localhost;dbname=testdrive',
             'emulatePrepare' => true,
-            'username' => 'root',
+            'username' => '',
             'password' => '',
             'charset' => 'utf8',
         ),
-        */
         'errorHandler' => array(
             // use 'site/error' action to display errors
             'errorAction' => 'site/error',
