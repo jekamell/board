edd2ab7210ca1533c7e42b1e95990b46b2b5b26b
Evgeniy Malykhin <55jeka@gmail.com>
2014-11-15 00:51:23 +0200

    Change: Migration: additional field for know if user  confirm registration

--- protected/migrations/m141114_205039_add_user_table.php
+++ protected/migrations/m141114_205039_add_user_table.php
@@ -10,6 +10,7 @@ class m141114_205039_add_user_table extends CDbMigration
               `name` varchar(255) NOT NULL,
               `email` varchar(255) NOT NULL,
               `password` varchar(255) NOT NULL,
+              `is_confirmed` tinyint(1) default 0,
               `date_add` datetime NOT NULL,
               `date_update` datetime,
               PRIMARY KEY (`id`)
