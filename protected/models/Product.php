7c22fd6ab6bbc5a3099ce312f1927d7b94e96a32
Evgeniy Malykhin <55jeka@gmail.com>
2014-11-15 22:52:16 +0200

    Add: App: Product model

---
+++ protected/models/Product.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * This is the model class for table "product".
+ *
+ * The followings are the available columns in table 'product':
+ * @property string $id
+ * @property string $title
+ * @property string $price
+ * @property integer $is_deleted
+ * @property string $user_id
+ * @property string $date_add
+ * @property string $date_update
+ *
+ * The followings are the available model relations:
+ * @property User $user
+ */
+class Product extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'product';
+	}
+
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return Product the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return [
+            ['title, price', 'required'],
+			['is_deleted', 'numerical', 'integerOnly' => true],
+			['title', 'length', 'max' => 255],
+			['price', 'length', 'max' => 12],
+		];
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		return [
+			'user' => [self::BELONGS_TO, 'User', 'user_id'],
+		];
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id' => 'ID',
+			'title' => 'Title',
+			'price' => 'Price',
+			'is_deleted' => 'Is Deleted',
+			'user_id' => 'User',
+			'date_add' => 'Date Add',
+			'date_update' => 'Date Update',
+        ];
+	}
+}
