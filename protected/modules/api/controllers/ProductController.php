095e130f14ab06e3c770fda8a9be22caa7ff67db
Evgeniy Malykhin <55jeka@gmail.com>
2014-11-19 03:43:33 +0200

    Add: Api: delete/update actions for product

--- protected/modules/api/controllers/ProductController.php
+++ protected/modules/api/controllers/ProductController.php
@@ -10,7 +10,12 @@ class ProductController extends ApiController
                     'allow',
                     'users' => ['*'],
                     'actions' => ['list', 'index', 'view']
-                ]
+                ],
+                [
+                    'allow',
+                    'users' => ['@'],
+                    'actions' => ['update', 'delete', 'create'],
+                ],
             ],
             parent::accessRules()
         );
@@ -39,12 +44,43 @@ class ProductController extends ApiController
 
     public function actionUpdate($id)
     {
-        echo __METHOD__;
+        if ($model = $this->loadModelByIdAndUserId($id, Yii::app()->getUser()->getId())) {
+            $model->attributes = array(
+                'title' => $this->getParam('title'),
+                'price' => $this->getParam('price'),
+            );
+            if (isset($_FILES['image'])) {
+                $model->image = CUploadedFile::getInstanceByName('image');
+            }
+
+            if ($model->save()) {
+                $this->response->status = true;
+            } else {
+                $this->response->message = $model->getErrors();
+            }
+        } else {
+            throw new CHttpException(404, Response::NOT_FOUND);
+        }
+
     }
 
 
     public function actionDelete($id)
     {
-        echo __METHOD__;
+        if ($model = $this->loadModelByIdAndUserId($id, Yii::app()->getUser()->getId())) {
+            $model->is_deleted = 1;
+            if ($model->save()) {
+                $this->response->status = true;
+            } else {
+                $this->response->message = $model->getErrors();
+            }
+        } else {
+            throw new CHttpException(404, Response::NOT_FOUND);
+        }
+    }
+
+    protected function loadModelByIdAndUserId($id, $userId)
+    {
+        return Product::model()->findByAttributes(['id' => $id, 'user_id' => Yii::app()->getUser()->getId()]);
     }
 }
